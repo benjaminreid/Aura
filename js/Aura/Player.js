7ad2e98cf6ec86033f0c5d39c76f4df137c16575
Benjamin Reid <benjamin@nouveller.com>
2012-01-31 10:53:12 +0000

    Setup the proper tween

--- js/Aura/Player.js
+++ js/Aura/Player.js
@@ -5,6 +5,12 @@ AURA.Player = function(image) {
   this.widthMax = (AURA.Config.canvasWidth - this.width);
   this.width2   = this.width/2;
 
+  this.startX   = ((AURA.Config.canvasWidth / 2) - this.width2);
+  this.startY   = ((AURA.Config.canvasHeight - this.height) - 20);
+
+  this.initX    = this.startX;
+  this.initY    = this.startY + 100;
+
   this.position = new AURA.Vector2(0,0);
   this.vel      = new AURA.Vector2(0,0);
   this.temp     = new AURA.Vector2(0,0);
@@ -22,14 +28,14 @@ AURA.Player = function(image) {
 
 AURA.Player.prototype = {
   init: function() {
-    var x = ((AURA.Config.canvasWidth / 2) - this.width2),
-        y = ((AURA.Config.canvasHeight - this.height) - 20),
-        that = this;
-    that.position.reset(x,y);
+    var that = this;
+
+    that.position.reset(this.initX,this.initY);
+
     window.addEventListener('keydown', function(e) { that.keyDownHandler(e,that); }, true);
     window.addEventListener('keyup', function(e) { that.keyUpHandler(e,that); }, true);
     
-    that.startTween = new TWEEN.Tween(that.position).to({ x: 10, y: 50 }, 2000);
+    that.startTween = new TWEEN.Tween(that.position).to({ x: that.startX, y: that.startY }, 500);
     that.startTween.easing(TWEEN.Easing.Quadratic.EaseOut);
     that.startTween.start();
   },
